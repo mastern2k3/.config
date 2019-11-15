b448baa85100f930076dba1e435ad72bd56bb378
Nitzan Zada <nitzan.zada@gmail.com>
2019-11-15 23:53:53 +0200

    this fish stuff is awesome

--- omf/before.init.fish
+++ omf/before.init.fish
@@ -40,7 +40,73 @@ set fish_pager_color_description $nord10
 set fish_pager_color_progress $nord12
 set fish_pager_color_secondary $nord1
 
-alias gg "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --all --author-date-order"
+# set PATH so it includes user's private bin if it exists
+if test -d $HOME"/bin"
+    set PATH $PATH $HOME"/bin"
+end
+
+if test -d $HOME"/.local/bin"
+    set PATH $PATH $HOME"/.local/bin"
+end
+
+if test -d $HOME"/.yarn/bin"
+    set PATH $PATH $HOME"/.yarn/bin"
+end
+
+if test -d $HOME"/.sdkman/bin"
+    set PATH $PATH $HOME"/.sdkman/bin"
+end
+
+if test -d "/usr/local/go/bin"
+    set PATH $PATH "/usr/local/go/bin"
+end
+
+if test -d (go env GOPATH)"/bin"
+    set PATH $PATH (go env GOPATH)"/bin"
+end
+
+if test -d "/usr/local/maven/bin"
+    set PATH $PATH "/usr/local/maven/bin"
+end
+
+if test -d "/snap/bin"
+    set PATH $PATH "/snap/bin"
+end
+
+# git aliases
+alias glon='glola --author-date-order'
+alias gg="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --all --author-date-order"
+alias gpump='git push --set-upstream git@gitlab.com:nitzan.zada/(git rev-parse --show-toplevel | xargs basename).git (git rev-parse --abbrev-ref HEAD) && git remote add origin git@gitlab.com:nitzan.zada/(git rev-parse --show-toplevel | xargs basename).git && git fetch --all'
+
+# bookmarks
+alias ttom='open_command https://trello.com/b/SbyBrJxQ'
+alias tid='open_command https://trello.com/b/tZaiZ8ln/ideas'
+
+# misc aliases
+alias untargz='tar xzvf'
+alias whoport='sudo lsof -i -P -n | grep LISTEN'
+alias xo=open_command
+alias btt=bat
+alias bt='bat --paging=never'
+alias pls=sudo
+alias wch='watch -n 1'
+alias hig='history | grep'
+alias gitnitz='git config user.email "nitzan.zada@gmail.com" && git config user.name "Nitzan Zada"'
+alias gitnitzhalo='git config user.email "nitzan@halo-digital.com" && git config user.name "Nitzan Zada"'
+alias gomtm='git fetch . origin/master:master'
+alias ynk='yank-cli -- xsel -b'
+alias ynkk=yank-cli
+alias l='exa --time-style iso -la --git --group-directories-first --icons'
+alias ls='exa --time-style iso -a --git --group-directories-first --icons'
+alias tk=taskell
+# alias life='taskell ~/Dropbox/life.md'
+alias life='taskell /keybase/private/mastern2k3/notes/life.md'
+alias tmux='tmux -u'
+alias vi='nvim'
+alias rmr='rm -r'
+
+# disable python venv "(venv)" prompt since agnoster
+set VIRTUAL_ENV_DISABLE_PROMPT 1
 
 # display neofetch output instead of normal fish greeting
 function fish_greeting
